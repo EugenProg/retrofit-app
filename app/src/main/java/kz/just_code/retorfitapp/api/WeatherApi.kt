1bd2fec7f41183e801025e374f18bdae0b352a3b
genius <Evko90@Yandex.ru>
2023-11-09 20:33:07 +0600

    feature: add interceptor

--- app/src/main/java/kz/just_code/retorfitapp/api/WeatherApi.kt
+++ app/src/main/java/kz/just_code/retorfitapp/api/WeatherApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface WeatherApi {
 
-    @GET("current.json?key=bf4dc83bbe114579817143928230611")
+    @GET("current.json")
     suspend fun getCurrentWeather(
         @Query("q") city: String,
         @Query("lang") language: String
--- app/src/main/java/kz/just_code/retorfitapp/api/WeatherApiData.kt
+++ app/src/main/java/kz/just_code/retorfitapp/api/WeatherApiData.kt
@@ -1,5 +1,9 @@
 package kz.just_code.retorfitapp.api
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,11 +14,39 @@ object WeatherApiData {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(getInterceptors())
             .build()
     }
 
     fun getApi(): WeatherApi {
         return getRetrofit()
             .create(WeatherApi::class.java)
+    }
+
+    private fun getInterceptors(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(KeyInterceptor())
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+}
+
+class KeyInterceptor: Interceptor {
+    private val weatherApiKey = "bf4dc83bbe114579817143928230611"
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request().url
+            .newBuilder()
+            .addQueryParameter("key", weatherApiKey)
+            .build()
+
+        val request = chain.request()
+            .newBuilder()
+            .url(url)
+            .build()
+
+        return chain.proceed(request)
     }
 }
